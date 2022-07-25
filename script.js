367b641566de6ff959a36ad10e10e7d2933a05f2
Nathaniel Mesfen <>
2022-07-25 21:20:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  window.location.href = 'https://finalbusinessproject-nathanielmesfen.oakblack10.repl.co/join.html';
+});
